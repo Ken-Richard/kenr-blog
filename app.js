3e3e20ed9c019aff0bf33f8ec10febcf0556e681
Ken Richard <kenrichar@gmail.com>
2014-12-21 07:44:17 -0500

    Express

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-var connect = require('connect'),
-  app = connect.createServer(),
-  port = process.env.PORT;
+var express = require('express')
+var app = express()
+var port = port = process.env.PORT || 3000;
 
-app.use(connect.static(__dirname + '/public'));
-app.use(connect.compress());
+app.use(express.static(__dirname + '/public'));
 
-app.listen(port, function(){
-  console.log('Hexo is running on port %d', port);
+var server = app.listen(port, function () {
+	var host = server.address().address
+ 	var port = server.address().port
+ 	console.log('Listening at http://%s:%s', host, port)	
 });
+
+// var connect = require('connect'),
+//   app = connect.createServer(),
+  
+
+// app.use(connect.static(__dirname + '/public'));
+// app.use(connect.compress());
+
+// app.listen(port, function(){
+//   console.log('Hexo is running on port %d', port);
+// });
