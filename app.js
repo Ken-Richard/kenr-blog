14174327603fdb504577aa25feb8a194038fca9e
Ken Richard <kenrichar@gmail.com>
2014-12-21 07:48:27 -0500

    fighting heroku

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require('express')
 var app = express()
 var port = port = process.env.PORT || 3000;
 
+console.log('Starting...');
+
 app.use(express.static(__dirname + '/public'));
 
 var server = app.listen(port, function () {
-	var host = server.address().address
- 	var port = server.address().port
- 	console.log('Listening at http://%s:%s', host, port)	
+	var host = server.address().address;
+ 	var port = server.address().port;
+ 	console.log('Listening at http://%s:%s', host, port);
 });
 
 // var connect = require('connect'),
